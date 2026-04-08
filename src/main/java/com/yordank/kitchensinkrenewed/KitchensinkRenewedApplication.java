625277170a2362dc7754c8377e95c8b001e8fc21
yordan98 <penguinn2@mail.bg>
2026-04-09 01:25:33 +0300

    Initial Spring Boot setup with MongoDB configuration

---
+++ src/main/java/com/yordank/kitchensinkrenewed/KitchensinkRenewedApplication.java
@@ -0,0 +1,13 @@
+package com.yordank.kitchensinkrenewed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchensinkRenewedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitchensinkRenewedApplication.class, args);
+    }
+
+}
